e77a594d4091d7e1b586abfdceb08ef7dd9ca713
youssef-erremili <yousseferremili19@gmail.com>
2025-02-15 22:12:18 +0100

    Edit Javascript

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,47 @@
-import './bootstrap';
+let buttons = document.querySelectorAll('.fetch')
+let containers = document.querySelectorAll('.data')
+let copyBtn = document.querySelector('#copyBtn')
+let alert = document.querySelector('#alert')
+
+
+if (buttons) {
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            removeActiveClass()
+            containers[index].classList.remove('hidden')
+        })
+    });
+
+    function removeActiveClass() {
+        containers.forEach((container) => {
+            container.classList.add("hidden");
+        })
+    }
+}
+
+if (copyBtn) {
+    copyBtn.addEventListener('click', () => {
+        let text = copyBtn.getAttribute('data');
+        async function copyContent() {
+            try {
+                await navigator.clipboard.writeText(text);
+                alert.classList.remove('hidden')
+                hideAlert()
+            }
+            catch (error) {
+                console.log('Failed to copy');
+            }
+        }
+        copyContent()
+    })
+}
+
+function hideAlert() {
+    if (alert) {
+        setTimeout(() => {
+            alert.classList.add('hidden')
+        }, 5000);
+    }
+}
+
+
