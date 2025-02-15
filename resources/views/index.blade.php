dadcd21418b132eff73120127955c814447f6c5b
youssef-erremili <yousseferremili19@gmail.com>
2025-02-15 22:24:21 +0100

    Create Index for our API

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <main class="w-10/12 mx-auto mt-32">
+        <span class="text-[#00bc51] font-medium">#DimaRaja</span>
+        <div>
+            <h1 class="text-7xl capitalize my-2 font-bold text-[#00bc51]">raja athletic club rest api</h1>
+            <p class="text-gray-800 font-medium inline-block mt-3">Stay informed about Raja Club’s rich history, including their league titles, cup victories, and other significant achievements. Keep up with the club’s success in both domestic and international competitions, and follow their journey towards continued excellence</p>
+        </div>
+        <div class="mt-5">
+            <a href="/docs" class="inline-block py-3 px-8 text-lg text-white rounded-lg bg-[#00bc51]">Documentation</a>
+            <a href="/" class="inline-block py-2.5 px-8 text-lg text-[#00bc51] rounded-lg border-2 border-[#00bc51] ml-4">Github</a>
+        </div>
+    </main>
+</x-layout>
