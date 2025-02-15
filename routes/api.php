a4898f6fd1c5a145cbcbe118d0de796144f3ed87
youssef-erremili <yousseferremili19@gmail.com>
2025-02-15 22:12:48 +0100

    Create api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ApiKeyController;
+use App\Http\Controllers\ClubController;
+use App\Http\Middleware\ValidateApiKey;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Route::get('/generate-api-key', [ApiKeyController::class, 'GenerateApiKey']);
+
+Route::middleware(ValidateApiKey::class)->prefix('/raja-club/{apikey}')->group(function () {
+    Route::get('/club-information', [ClubController::class, 'index']);
+    Route::get('/players', [ClubController::class, 'players']);
+    Route::get('/player/{id}', [ClubController::class, 'player']);
+    Route::get('/championships', [ClubController::class, 'championships']);
+    Route::get('/championship/{id}', [ClubController::class, 'championship']);
+});
