8c04378b3fce95620e609264c16c0cc879dd9d7a
youssef-erremili <yousseferremili19@gmail.com>
2025-02-15 22:13:23 +0100

    Create application routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClubController;
+use App\Http\Controllers\DocController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/sign-in', [AuthController::class, 'create']);
+Route::post('/auth-sign-in', [AuthController::class, 'login'])->middleware('guest');
+
+Route::get('/docs', [DocController::class, 'index']);
+
+Route::get('http://127.0.0.1:8000/api/raja-club/rIl5fXm7LWGQGClH9ukVaG5kqbkdmeHQlzapti1p/player/1', [ClubController::class, 'player']);
